bcbdf198a04906c425862ecd06be362c55c40b47
Igoxium <105248897+Igoxium@users.noreply.github.com>
2022-06-24 18:05:19 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1656104640,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/messagemgame.png",
+		"images/player-sheet0.png",
+		"images/btn_play-sheet0.png",
+		"images/cam-sheet0.png",
+		"images/arma1-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/bullet-sheet1.png",
+		"images/sangue.png",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
